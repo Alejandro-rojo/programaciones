1093d03968f7c9c85209ad8e234d14708727050d
unknown <juantamayomanzano@gmail.com>
2025-07-09 23:29:46 -0500

    sena

---
+++ ejercicios_con condicionales_y_operaciones_matematicas.py
@@ -0,0 +1,109 @@
+#1-verifica si el numero es positivo, negativo o cero 
+# numero=float(input("ingresa un numero: "))
+# if numero<0:
+#     print(f"{numero} es negativo")
+# elif numero>0:
+#     print(f"{numero} es positivo")
+# else:
+#     print(f"{numero} es cero")
+
+# #2-verifica el mayor de dos numero ingresados
+# num1=float(input("ingresa un numero: "))
+# num2=float(input("ingresa otro  numero: "))
+# if num1>num2:
+#     print(F"{num2} es menor que {num1}")
+# elif num1<num2:
+#     print(F"{num1} es menor que {num2}")
+# else:
+#     print(f"{num1} y {num2} son iguales")
+
+#3-determina si un numero es par o impar
+# num=int(input("ingresa un numero entero: "))
+# if num%2==0:
+#     print(f"{num} es par")
+# else:
+#     print(f"{num} es impar")
+
+#4-dado un numero, verifica si esta entre 10 y 20
+# num1=float(input("ingresa un numero: "))
+# if num1-10<=0:
+#     print(f"{num1} no esta entre 10 y 20 ")
+# elif num1+20>40:
+#     print(F"{num1} no esta entre 20 y 10")
+# elif num1==0:
+#     print(f"el cero no esta entre 20 y 10")
+# elif num1==20:
+#     print(F"{num1} no esta entre 20 y 10 porque no abarcamos los limites")
+# elif num1==10:
+#     print(F"{num1} no esta entre 20 y 10 porque no abarcamos los limites")
+# else:
+#     print(f"{num1} esta entre 20 y 10")
+
+#5-dado 3 numeros, encuentra el mayor usando condicionales
+# num1=float(input("ingresa un numero: "))
+# num2=float(input("ingresa un numero: "))
+# num3=float(input("ingresa un numero: "))
+# if num1*3==num2+num3+num1:
+#     print(f"todos son iguales")
+# elif num1+num2<num3:
+#     print(f"{num3} es el mayor")
+# elif num1+num3<num2:
+#     print(f"{num2} es el mayor")
+# elif num3+num2<num1:
+#     print(f"{num1} es el mayor")
+# elif num1>num2>num3:
+#     print(F"{num1} es mayor que todos")
+# elif num1>num3>num2:
+#     print(F"{num1} es mayor que todos")
+# elif num2>num1>num3:
+#     print(F"{num2} es mayor que todos")
+# elif num2>num3>num1:
+#     print(F"{num2} es mayor que todos")
+# elif num3>num1>num2:
+#     print(F"{num3} es mayor que todos")
+# elif num3>num2>num1:
+#print(F"{num3} es mayor que todos")
+
+# #6-calcula el precio final con un 10% de descuento si el total el mayor que $100
+# producto1=int(input("dame el primer precio: "))
+# produto2=int(input("dame el segundo precio: "))
+# total=producto1+produto2
+# if produto2+producto1>100:
+#     total1=total-0.1*total
+#     print(total1)
+# else:
+#     print("no hay descuento mi bro")
+
+#7-verifica si una persona puede votar
+# edad=int(input("ingresa tu edad: "))
+# if edad>=18:
+#     print("eres mayor de edad")
+# else:
+#     print("eres menor de edad")
+
+#8-Dado el precio y tipo de cliente (VIP o normal), aplica un descuento del 20% solo a VIP
+# precio=int(input("cuanto gastaste: "))
+# tipo=int(input("eres vip? (si es 1/no es 2): "))
+# total1=precio-precio*0.2
+# if tipo==1:
+#     print(f"tienes descuento y por eso tienes que pagar {total1}")
+# else:
+#     print(f"te toca pagar {precio}")
+
+#9-Determina si un número es múltiplo de 5 y de 3 al mismo tiempo.
+# num1=int(input("ingresa un numero: "))
+# if num1%3==0 and num1%5==0:
+#     print(f"{num1} es multiplo de 3 y 5 ")
+# else:
+#     print(f"{num1} no es multiplo de 3 y 5 al mismo tiempo")
+
+#10-Dado un número, verifica si es divisible entre dos números dados.
+# num1=float(input("ingresa un numero: "))
+# num2=float(input("ingresa un numero: "))
+# num3=float(input("ingresa un numero: "))
+# if num1%num2==0 and num1%num3==0:
+#     print({num1} es divisible entre {num2} y {num3})
+# else:
+#     print("los numero no son divisible")
+#perdon 
+n="hota"
