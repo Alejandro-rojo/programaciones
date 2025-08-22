cc523c96db108f731b0ffd4bba13b9ea25dba9bc
Fabian Salazar <vxlegend47@gmail.com>
2025-08-22 17:36:59 -0500

    sergey

---
+++ bucles(while).py
@@ -0,0 +1,56 @@
+# numero=float(input("ingresa un numero: "))
+# while numero>0:
+#     print(f"{numero}")
+#     numero-=1
+# print("termino el conteo")
+
+# numero1=int(input("ingresa numero de la tabla de multiplicar: "))
+# i=1
+# print(f"inicia la tabla del {numero1}")
+# while i<=10:
+#     print(f"{numero1} * {i}={numero1*i}")
+#     i+=1
+
+# x=0
+# while True:
+#     x-=1
+#     print(x)
+#     if x==0:
+#         break
+#     print("fin del bucle")
+
+# #EJERCICIO 1
+# cuenta=0#primero creamos la variable a la cual se le va a sumar los numero ingresados
+# while True:#empezamos
+#     numero=int(input("ingresa un numero entero: "))
+#     cuenta+=numero#a cuenta se le suma el numero ingresado
+#     if numero==0:
+#         break
+# print(f"la suma de los numeros que ingresaste es {cuenta}")
+
+# #EJERCICIO 2
+# contraseña=""
+# while contraseña!="python123":
+#     contraseña=input("ingresa la contraseña: ")
+# print("contraseña correcta")
+
+# #EJERCICIO 3
+# cuenta=[]
+# while True:
+#     producto=input("ingresa el producto: ")
+#     if producto=="fin":
+#         break
+#     cuenta.append(producto)
+# print(f"en total pediste {cuenta}")
+
+#EJERCICIO 4
+numero1=int(input("ingresa un numero entero: "))
+numero2=int(input("ingresa un numero entero: "))
+numero3=int(input("ingresa un numero entero: "))
+numero4=int(input("ingresa un numero entero: "))
+numero5=int(input("ingresa un numero entero: "))
+numero6=int(input("ingresa un numero entero: "))
+numero7=int(input("ingresa un numero entero: "))
+numero8=int(input("ingresa un numero entero: "))
+numero9=int(input("ingresa un numero entero: "))
+numero10=int(input("ingresa un numero entero: "))
